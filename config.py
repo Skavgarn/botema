e25c4f152b73cae3c424bbf10379b20e58a1c049
Skavgarn <slovgarn@mail.ru>
2023-03-05 22:44:02 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+api_key = '' #вводим api_key
+secret_key = '' # вводим secret_key
+symbol = "LTCUSDT" # вводим пару
+interval = "1m" #тайм фрейм, примеры:1m, 5m , 15m , 1h ,1d , 1w
+quantity = 3 # сумма входа в долларах
+short_ema = 8 # короткая скользящая
+long_ema = 21  # длинная скользящая
+leverage = 10 # кредитное плечо
